f01f958f5c2f8f799e3499cade0bb99e05af7334
Moritz, David <Moritz.David@principal.com>
2016-03-25 15:35:11 -0500

    test google  sign-in

--- src/js/main.js
+++ src/js/main.js
@@ -68,6 +68,22 @@ mainApp.controller('MainCtrl', [
 			return new Date(now.setMinutes(offset));
 		}
 
+		// $s.onSignIn = function(googleUser) {
+		// 	console.log('test');
+		// 	var profile = googleUser.getBasicProfile();
+		// 	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+		// 	console.log('Name: ' + profile.getName());
+		// 	console.log('Image URL: ' + profile.getImageUrl());
+		// 	console.log('Email: ' + profile.getEmail());
+		// };
+
+		$s.signOut = function() {
+			var auth2 = gapi.auth2.getAuthInstance();
+			auth2.signOut().then(function () {
+				console.log('User signed out.');
+			});
+		};
+
 		$s.sameTime = function() {
 			$s.ballot.resultsRelease = new Date($s.ballot.voteCutoff.getTime());
 		};
