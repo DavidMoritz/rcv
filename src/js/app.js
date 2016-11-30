daf13b620295964385f1e933a3fb0f56e190ab88
David Moritz <davidmoritz@gmail.com>
2016-11-30 16:00:56 -0600

    temporarily remove facebook

--- src/js/app.js
+++ src/js/app.js
@@ -38,10 +38,10 @@ window.fbAsyncInit = function() {
 	});
 };
 
-(function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement(s); js.id = id;
-	js.src = '//connect.facebook.net/en_US/sdk.js';
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+// (function(d, s, id){
+// 	var js, fjs = d.getElementsByTagName(s)[0];
+// 	if (d.getElementById(id)) {return;}
+// 	js = d.createElement(s); js.id = id;
+// 	js.src = '//connect.facebook.net/en_US/sdk.js';
+// 	fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
